b80775600319636eae8443eef3ea81399143d735
teebin2019 <teebin2019@gmail.com>
2025-07-03 15:24:25 +0700

    update verify

--- connent/db.php
+++ connent/db.php
@@ -1,15 +1,14 @@
 <?php
-require '../vendor/autoload.php';
+require   __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-$my_secret = $_ENV['SECRET_KEY'];
-echo $my_secret;
 
 $servername = $_ENV['SERVERNAME'];
 $username = $_ENV['USERNAME'];
 $password = $_ENV['PASSWORD'];
 $dbname = $_ENV['DATABASE'];
+$apikey = $_ENV['API_KEY'];
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
--- dashboard.php
+++ dashboard.php
@@ -23,7 +23,7 @@ if (!$_SESSION) {
                 <div class="card-text">Welcome to <?= $_SESSION['username']; ?></div>
             </div>
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">กรุณาตรวจสอบการชำระเงิน</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
                     card’s content.</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
